0d51293131619b07a773540d5880b525b01b5bb7
Ethan Gardner <ethan.gardner@gsa.gov>
2026-05-21 17:47:34 -0400

    use func for token output format

--- config/style-dictionary.config.js
+++ config/style-dictionary.config.js
@@ -17,28 +17,23 @@ StyleDictionary.registerTransform({
   transform: getTokenValueWithUnit,
 });
 
+function makePlatform(format) {
+  return {
+    transforms: ["name/uswds-theme", "value/uswds-units"],
+    prefix: "usa",
+    buildPath: `build/${format}/`,
+    files: tokenGroups.map((group) => ({
+      destination: format === "scss" ? `_${group}.scss` : `${group}.css`,
+      format: `${format}/variables`,
+      filter: (token) => token.filePath.includes(`tokens/${group}/`),
+    })),
+  };
+}
+
 export default {
   source: ["tokens/**/*.json"],
   platforms: {
-    scss: {
-      transforms: ["name/uswds-theme", "value/uswds-units"],
-      prefix: "usa",
-      buildPath: "build/scss/",
-      files: tokenGroups.map((group) => ({
-        destination: `_${group}.scss`,
-        format: "scss/variables",
-        filter: (token) => token.filePath.includes(`tokens/${group}/`),
-      })),
-    },
-    css: {
-      transforms: ["name/uswds-theme", "value/uswds-units"],
-      prefix: "usa",
-      buildPath: "build/css/",
-      files: tokenGroups.map((group) => ({
-        destination: `${group}.css`,
-        format: "css/variables",
-        filter: (token) => token.filePath.includes(`tokens/${group}/`),
-      })),
-    },
+    scss: makePlatform("scss"),
+    css: makePlatform("css"),
   },
 };
